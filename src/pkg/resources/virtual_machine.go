12a900dcdc770b411de4e085bb659b7bcf32fcb5
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-10-04 21:53:16 -0500

    vm

--- src/pkg/resources/virtual_machine.go
+++ src/pkg/resources/virtual_machine.go
@@ -1,30 +1,28 @@
 package resources
 
 import (
-	"fmt"
-
 	"github.com/aws/jsii-runtime-go"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
+	lvm "github.com/transprogrammer/xenia/generated/hashicorp/azurerm/linuxvirtualmachine"
 	nic "github.com/transprogrammer/xenia/generated/hashicorp/azurerm/networkinterface"
 	rg "github.com/transprogrammer/xenia/generated/hashicorp/azurerm/resourcegroup"
-	vm "github.com/transprogrammer/xenia/generated/hashicorp/azurerm/virtualmachine"
 	"github.com/transprogrammer/xenia/generated/naming"
 	"github.com/transprogrammer/xenia/pkg/cfg"
 )
 
-func NewVirtualMachine(stack cdktf.TerraformStack, cfg cfg.Config, naming naming.Naming, rg rg.ResourceGroup, nic nic.NetworkInterface) vm.VirtualMachine {
+func NewVirtualMachine(stack cdktf.TerraformStack, cfg cfg.Config, naming naming.Naming, rg rg.ResourceGroup, nic nic.NetworkInterface) lvm.LinuxVirtualMachine {
 
-	storageImageReference := vm.VirtualMachineStorageImageReference{
+	imageReference := lvm.LinuxVirtualMachineSourceImageReference{
 		Publisher: jsii.String("Canonical"),
 		Offer:     jsii.String("0001-com-ubuntu-server-jammy"),
 		Sku:       jsii.String("22_04-lts-gen2"),
 		Version:   jsii.String("latest"),
 	}
 
-	storageOSDisk := vm.VirtualMachineStorageOsDisk{
-		Name:            jsii.String("osdisk"),
-		CreateOption:    jsii.String("FromImage"),
-		ManagedDiskType: jsii.String("Standard_LRS"),
+	osDisk := lvm.LinuxVirtualMachineOsDisk{
+		Name:               jsii.String("osdisk"),
+		Caching:            jsii.String("ReadWrite"),
+		StorageAccountType: jsii.String("Standard_LRS"),
 	}
 
 	networkInterfaceIds := []*string{nic.Id()}
@@ -37,43 +35,31 @@ func NewVirtualMachine(stack cdktf.TerraformStack, cfg cfg.Config, naming naming
 	}
 	adminUsername := cdktf.NewTerraformVariable(stack, adminId, &adminConfig)
 
-	keyId := jsii.String("key_data")
+	keyId := jsii.String("public_key")
 	keyConfig := cdktf.TerraformVariableConfig{
 		Type:        jsii.String("string"),
-		Description: jsii.String("administrator username"),
+		Description: jsii.String("public key"),
 		Sensitive:   jsii.Bool(true),
 	}
 	keyData := cdktf.NewTerraformVariable(stack, keyId, &keyConfig)
 
-	osProfile := vm.VirtualMachineOsProfile{
-		ComputerName:  naming.VirtualMachineOutput(),
-		AdminUsername: adminUsername.StringValue(),
-		AdminPassword: keyData.StringValue(),
+	sshKey := lvm.LinuxVirtualMachineAdminSshKey{
+		Username:  adminUsername.StringValue(),
+		PublicKey: keyData.StringValue(),
+	}
+	sshKeys := []*lvm.LinuxVirtualMachineAdminSshKey{&sshKey}
+
+	input := lvm.LinuxVirtualMachineConfig{
+		AdminUsername:        adminUsername.StringValue(),
+		Name:                 naming.LinuxVirtualMachineOutput(),
+		Location:             cfg.Regions().Primary(),
+		ResourceGroupName:    rg.Name(),
+		Size:                 jsii.String("Standard_B2ms"),
+		SourceImageReference: &imageReference,
+		OsDisk:               &osDisk,
+		NetworkInterfaceIds:  &networkInterfaceIds,
+		AdminSshKey:          &sshKeys,
 	}
 
-	sshPath := fmt.Sprintf("/home/%s/.ssh/authorized_keys", adminUsername.StringValue())
-
-	sshKeys := vm.VirtualMachineOsProfileLinuxConfigSshKeys{
-		Path:    &sshPath,
-		KeyData: keyData.StringValue(),
-	}
-
-	osProfileLinuxConfig := vm.VirtualMachineOsProfileLinuxConfig{
-		DisablePasswordAuthentication: jsii.Bool(true),
-		SshKeys:                       sshKeys,
-	}
-
-	input := vm.VirtualMachineConfig{
-		Name:                  naming.VirtualMachineOutput(),
-		Location:              cfg.Regions().Primary(),
-		ResourceGroupName:     rg.Name(),
-		VmSize:                jsii.String("Standard_B2ms"),
-		StorageImageReference: &storageImageReference,
-		StorageDataDisk:       &storageOSDisk,
-		NetworkInterfaceIds:   &networkInterfaceIds,
-		OsProfile:             &osProfile,
-		OsProfileLinuxConfig:  &osProfileLinuxConfig,
-	}
-
-	return vm.NewVirtualMachine(stack, Ids.VirtualMachine, &input)
+	return lvm.NewLinuxVirtualMachine(stack, Ids.VirtualMachine, &input)
 }
