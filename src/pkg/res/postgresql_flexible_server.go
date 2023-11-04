9cc09813983fbcea89598480df2b1487301cc6a9
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-11-04 17:29:57 -0500

    chk

--- src/pkg/res/postgresql_flexible_server.go
+++ src/pkg/res/postgresql_flexible_server.go
@@ -3,7 +3,7 @@ package res
 import (
 	"github.com/aws/jsii-runtime-go"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
-	pg "github.com/transprogrammer/xenia/generated/hashicorp/azurerm/postgresflexibleserver"
+	pg "github.com/transprogrammer/xenia/generated/hashicorp/azurerm/postgresqlflexibleserver"
 	dns "github.com/transprogrammer/xenia/generated/hashicorp/azurerm/privatednszone"
 	rg "github.com/transprogrammer/xenia/generated/hashicorp/azurerm/resourcegroup"
 	vnet "github.com/transprogrammer/xenia/generated/hashicorp/azurerm/virtualnetwork"
@@ -14,11 +14,10 @@ import (
 // PORT: Using postgres server naming as stand-in. <>
 // ???: Add flexi-server resource definition to naming fork? <rbt>
 
-var PostgresFlexibleServerVersion = jsii.String("15")
-var PostgresFlexibleServerStorageMB = jsii.Number(32768)
-var PostgresFlexible
-
 func NewPostgresFlexibleServer(stack cdktf.TerraformStack, cfg cfg.Config, naming naming.Naming, rg rg.ResourceGroup, subnet vnet.VirtualNetworkSubnetOutputReference, zone dns.PrivateDnsZone, tenantId *string) pg.PostgresFlexibleServer {
+
+	serverVersion := jsii.String("15")
+	storageMB := jsii.Number(32768)
 
 	auth := pg.PostgresqlFlexibleServerAuthentication{
 		ActiveDirectoryAuthEnabled: jsii.Bool(true),
@@ -30,10 +29,11 @@ func NewPostgresFlexibleServer(stack cdktf.TerraformStack, cfg cfg.Config, namin
 		Name:              naming.PostgresqlServerOutput(),
 		ResourceGroupName: rg.Name(),
 		Location:          cfg.Regions().Primary(),
-		Version:           PostgresFlexibleServerVersion,
+		Version:           serverVersion,
 		DelegatedSubnetId: subnet.Id(),
-		PrivateDNSZoneId:  zone.Id(),
+		PrivateDNSZone:    zone.Id(),
 		Authentication:    auth,
+		StorageMb:         storageMB,
 	}
 
 	return pg.NewPostgresFlexibleServer(stack, Ids.PostgresFlexibleServer, &input)
